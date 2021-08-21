429c286862a518b51ae1f7a6434376b525f33a2e
Felipe <felipescherer25@gmail.com>
2021-08-21 15:59:37 -0300

    feat: Integrations weapons

--- src/app/components/weapons/weapons.component.ts
+++ src/app/components/weapons/weapons.component.ts
@@ -11,58 +11,10 @@ import { WeaponDialogComponent } from './weapon-dialog/weapon-dialog.component';
   styleUrls: ['./weapons.component.scss']
 })
 export class WeaponsComponent implements OnInit {
-  @Input() data: string;
+  @Input() data: Array<weaponInterface>;
 
   constructor(private dialog: MatDialog) { }
   hexDialogRef: MatDialogRef<WeaponDialogComponent>;
-  weapons: Array<weaponInterface> = [];
-  //   {
-  //     type: 'melee',
-  //     class: 'D',
-  //     title: "Faca flamejante",
-  //     weaponType: "Faca",
-  //     description: "uma faca pegando fogo",
-  //     damage: "d6",
-  //     range: "Curto",
-  //     rateOfFire: [],
-  //     imageUrl: "https://static.remove.bg/remove-bg-web/3661dd45c31a4ff23941855a7e4cedbbf6973643/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png",
-  //   },
-  //   {
-  //     type: 'ranged',
-  //     class: 'D',
-  //     title: "Pistola a laser",
-  //     weaponType: "Pistola",
-  //     description: "Uma pistola que solta laser",
-  //     damage: "d8",
-  //     range: "Médio",
-  //     rateOfFire: [true,true,true,false,true],
-  //     imageUrl: "",
-  //   },
-  //   {
-  //     type: 'melee',
-  //     class: 'D',
-  //     title: "Faca flamejante",
-  //     weaponType: "Faca",
-  //     description: "uma faca pegando fogo",
-  //     damage: "d6",
-  //     range: "Curto",
-  //     rateOfFire: [],
-  //     imageUrl: "https://static.remove.bg/remove-bg-web/3661dd45c31a4ff23941855a7e4cedbbf6973643/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png",
-  //   },
-  //   {
-  //     type: 'melee',
-  //     class: 'D',
-  //     title: "Faca flamejante",
-  //     weaponType: "Faca",
-  //     description: "uma faca pegando fogo",
-  //     damage: "d6",
-  //     range: "Curto",
-  //     rateOfFire: [],
-  //     imageUrl: "https://static.remove.bg/remove-bg-web/3661dd45c31a4ff23941855a7e4cedbbf6973643/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png",
-  //   },
-    
-
-  // ];
 
   ngOnInit(): void {
 
@@ -73,7 +25,7 @@ export class WeaponsComponent implements OnInit {
     this.hexDialogRef.afterClosed()
       .pipe(filter(data => data))
       .subscribe(data => {
-        this.weapons.push(data);
+        this.data.push(data);
       });
   }
 
@@ -82,9 +34,9 @@ export class WeaponsComponent implements OnInit {
   }
 
   removeWeapon(weapon: weaponInterface): void {
-    const index = this.weapons.indexOf(weapon);
+    const index = this.data.indexOf(weapon);
     if (index > -1) {
-      this.weapons.splice(index, 1);
+      this.data.splice(index, 1);
     }
   }
 }
