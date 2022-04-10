bcc29609a6e29e512a7004537aef50c35edf1976
Felipe Scherer <felipescherer25@gmail.com>
2022-04-10 15:15:33 -0300

    feat: tempaltes

--- src/app/components/templates/data.ts
+++ src/app/components/templates/data.ts
@@ -5,7 +5,23 @@ export const dataList = [
     value: 'samyaza',
     title: 'Angel of Pride',
     description: 'Samyaza é o líder dos Watchers.\n Foi o primeiro a receber seu nome',
-    powers: [],
+    powers: [
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1617135050172-PKFS8WTG3O5XTNYK1VH3/Samyaza.jpg?format=500w',
   },
@@ -15,7 +31,23 @@ export const dataList = [
     value: 'azazel',
     title: 'Angel of Sacrifices',
     description: 'lorem',
-    powers: [],
+    powers: [
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1617135069197-JKK8SS1R8315T7E3UKTH/Azazel.jpg?format=500w',
   },
@@ -25,7 +57,23 @@ export const dataList = [
     value: 'sariel',
     title: 'Angel of the Waning Moon',
     description: 'lorem',
-    powers: [],
+    powers: [
+      {
+        title: 'Te dou meu sangue',
+        cost: 'Qualquer D até o D8 de estabilidade',
+        desc: 'Sariel não gosta de se machucar, mas é orgulhoso de mais para reprimir o desejo do usuário. Você pode curar alguém da sua party jogando qualquer D até um D8, o membro da party receberá a metade + 2 de vida',
+      },
+      {
+        title: 'Cura sobrenatural',
+        cost: 'D8 de estabilidade',
+        desc: 'Sariel não morrerá. Toda vez que você sofrer um dano, ele pode ser curado jogando esse dado',
+      },
+      {
+        title: 'Personificação do orgulho',
+        cost: 'D8 de estabilidade para cada vez lançado',
+        desc: 'Anula qualquer poder lançado a você, todos os que te atacarem receberam uma desvantagem e dano reduzido em 2',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1454542062623-QU93WAHZDPKATGWZW0DV/sariel_watchers_grigori_moon_Mohrbacher?format=500w',
   },
@@ -36,7 +84,23 @@ export const dataList = [
     title: 'Angel of Lightining',
     description:
       'Não me toque\nSeu demônio, seu terror\nNem por um momento\n\nVocê mudou minha mente\nVocê parou meu coração\nSeu demônio, seu terror\nEu quero que você me toque novamente\n\nNão me tente\nNão me tente\nEu quero ver você marchar novamente',
-    powers: [],
+    powers: [
+      {
+        title: 'Carga',
+        cost: 'D4 de estabilidade para cada D6 de dano, podendo no máximo 4D4',
+        desc: 'Baraqiel é insano, tudo que ele deseja é queimar tudo. Você pode trocar estabilidade por dano',
+      },
+      {
+        title: 'Energização',
+        cost: '2D4 de estabilidade para deixar sua arma com D12 a mais de dano por D4 rodadas',
+        desc: 'Minha arma solta raios e trovões. Você pode energizar uma arma de sua preferência',
+      },
+      {
+        title: 'Manipulação da Eletricidade',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1617135237947-2SWVXU20L3UBJDYJ8HJZ/baraqiel.jpg?format=500w',
   },
@@ -46,7 +110,23 @@ export const dataList = [
     value: 'af',
     title: 'Angel of Anger',
     description: 'Bem-vindo\nComo epifania\n\nSolte-o\nComo a canção\n\nHonre o que você ama\nCom ruína',
-    powers: [],
+    powers: [
+      {
+        title: 'Troca Equivalente',
+        cost: '2D4',
+        desc: 'Af te dá o que você mais quer, ela só pede uma coisa em troca... Que você mate todos.\n Durante 3 rodadas você conseguirá invocar um ser para te auxiliar no combate. Ele faz com que você fique com inciativa prioritária e pode fazer duas ações tanto normais quanto de combate.',
+      },
+      {
+        title: 'Redução de dano',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1465259667157-6VO479Q1FOPUH3DNTKMH/Af.jpg?format=500w',
   },
@@ -55,8 +135,25 @@ export const dataList = [
     displayName: 'Penemue',
     value: 'penemue',
     title: 'Angel of Written Word',
-    description: 'lorem',
-    powers: [],
+    description:
+      'O que foi feito\nDo outro lado da colina de fogo\n\nEssas esculturas de homens\nCujos nomes os anjos esqueceram',
+    powers: [
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: 'Viva',
+        cost: 'D6',
+        desc: 'Gasta uma página do caderno, mas consegue invocar qualquer coisa desenhada nela',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1465256923958-C6QB6GCCOJTJRUM4PAPJ/PenemueF.jpg?format=500w',
   },
@@ -67,7 +164,23 @@ export const dataList = [
     title: 'Angel of Shadows',
     description:
       'Caindo pelos galhos\nParado entre as folhas trêmulas\nPare e ouça\n\nMinha abordagem,\nmeu toque.\n\nO vento muda e lá estou eu\nfresco contra sua pele.\nEu não vou te deixar.',
-    powers: [],
+    powers: [
+      {
+        title: 'Invocação de seres da sombra',
+        cost: 'D4 para cada ser',
+        desc: 'Bezaliel é um rei, e por isso manda os outros fazerem. O jogador pode escolher quantos seres da sombra serão invocados com máximo de 4 seres. Eles podem realizar ações simples (Inclusive deverá ser tirado uma iniciativa para cada) e o dano de cada um vai variar com um D3. Obs: O jogador precisa escolher quantos seres serão invocados antes de jogar os dados.',
+      },
+      {
+        title: 'Reanimação dos mortos',
+        cost: '2D4 + 1D2',
+        desc: 'Ao encontrar um ser morto, o portador de Bezaliel pode tentar reanimá-lo e fazer esse ser se juntar ao ser exército. Os stats dele serão combinados com o mestre posteriormente no jogo.',
+      },
+      {
+        title: 'Exército da sombra',
+        cost: '2D4 + 1D2',
+        desc: 'Bezaliel conta com um exército escondido. Você agora pode usar ele.',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1462028406480-13LRA35CMV1M68T8E66A/bezalielF.jpg?format=500w',
   },
@@ -77,7 +190,23 @@ export const dataList = [
     value: 'chazaqiel',
     title: 'Angel of Mists',
     description: 'lorem',
-    powers: [],
+    powers: [
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1456773844384-TGKEF3LTX8YSEH44DLAZ/chazaqielF.jpg?format=500w',
   },
@@ -88,7 +217,23 @@ export const dataList = [
     title: 'Angel of Ruin',
     description:
       'Onde você acabou de ir?\nVocê estava aqui conosco, e então\nNós não vimos você sair\n\nNo vento ouço meu nome\nEles cantam minha história\nDe braços abertos, eles esperam\nchamando-me para me juntar a eles.\n\nOh, para ver seu rosto novamente\nBem vindo amigo\nVocê esteve longe por muito tempo\nQue contos você traz?\nQue sabedoria?\n\nO amor que tínhamos\nEle nunca se desvanece\nEssas horas douradas, me ligue\nEles dizem meu nome',
-    powers: [],
+    powers: [
+      {
+        title: 'Escudo de areia',
+        cost: '8D2',
+        desc: 'Asbeel protegerá sua rainha a qualquer custo (Inclusive a estabilidade dela). Esse poder pode ser usado quando algum mostro atacar ela e bloqueia 100% do dano que seria sofrido.',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1465259734105-ESQDPZZA3Y9W6CU82504/asbeelF.jpg?format=500w',
   },
@@ -98,7 +243,23 @@ export const dataList = [
     value: 'kokabiel',
     title: 'Angel of the Stars',
     description: 'lorem',
-    powers: [],
+    powers: [
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+      {
+        title: '',
+        cost: '',
+        desc: '',
+      },
+    ],
     image:
       'https://images.squarespace-cdn.com/content/v1/54243095e4b07ee637a9f812/1465258873952-3CHIREOLHKSQPWQZRNGW/kokabiel_watchers_grigori_star_light_fallen_angel_Mohrbacher?format=500w',
   },
